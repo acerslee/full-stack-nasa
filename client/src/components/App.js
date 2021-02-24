f9c21b383832b84200c24242decf3c288c32ceed
Alex Lee <ahjlee712@gmail.com>
2021-02-24 18:01:22 -0500

    Completed part 1

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import KEY from '../../../key.js';
 
 export default class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      message: "Hello HRATX52"
+      date: '',
+      explanation: '',
+      title: '',
+      picture: ''
     };
+  }
 
+  fetchAPOD() {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${KEY}`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        date: res.data.date,
+        explanation: res.data.explanation,
+        title: res.data.title,
+        picture: res.data.url
+      })
+    })
+    .catch(err => console.log(err));
+  }
+
+  componentDidMount(){
+    this.fetchAPOD()
   }
 
   render() {
     return (
       <div>
-        <p>{this.state.message}</p>
+        <p>{this.state.date}</p>
+        <p>{this.state.explanation}</p>
+        <p>{this.state.title}</p>
+        <img src = {this.state.picture}></img>
       </div>
     );
   }
